be947b0c77d234f7c6c358406ede2f5c15b7592d
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-28 21:12:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     time = time.hour
 
     if time.between?(0, 11) == true
-    resp.write "Good Morning"
+    resp.write "Morning"
   else
     resp.write "Good Afternoon"
   end
