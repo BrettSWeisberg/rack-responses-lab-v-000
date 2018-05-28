c5196737e8147ac765b0126dbc6667bfd7b81b76
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-28 21:07:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,11 @@ class Application
   def call(dev)
     binding.pry
     resp = Rack::Responses.new
-    
+
     time = Time.new
     time = time.hour
-
-    if time > 0 && time < 12
+  
+    if time.between?(0, 12)
     resp.write "Good Morning"
   else
     resp.write "Good Afternoon"
