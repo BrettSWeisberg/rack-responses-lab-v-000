6393932c2cbaa4b5a41dbe33d9c5e0963b1e9532
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-28 20:49:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,13 @@ class Application
 
   def call(dev)
     resp = Rack::Responses.new
+
+    if time > 0 && time < 12
     resp.write "Good Morning"
+  else
     resp.write "Good Afternoon"
+  end
     resp.finish
   end
-  
+
 end
