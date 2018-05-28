fa41118ef3da2821d020c6bb4839ad9e826902be
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-28 21:11:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     time = Time.new
     time = time.hour
 
-    if time.between?(0, 12) == true
+    if time.between?(0, 11) == true
     resp.write "Good Morning"
   else
     resp.write "Good Afternoon"
